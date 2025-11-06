e4014bdb3afa1f7c9b843a00fc3ad0a335a137f0
Omar Minero <omar.minero01@gmail.com>
2025-11-05 19:07:48 -0600

    Add files via upload

---
+++ operadores relacionales.dart
@@ -0,0 +1,27 @@
+void main(){
+int cantidad1 = 90, cantidad2 = 100;
+var palabra1 = 'universo', palabra2 = 'planeta';
+print(cantidad1);
+
+//operador de igualdad
+print(cantidad1 ==cantidad2);
+print('palabra: ${palabra1 == palabra2}');
+
+// operador de desigualdad
+print(cantidad1 !=cantidad2);
+
+//operador mayor que 
+print(cantidad1 > cantidad2);
+
+//operador menor que 
+print(cantidad1 < cantidad2);
+
+//operador mayor o igual que 
+print(cantidad1 >= cantidad2);
+
+//operador menor  o igual que 
+print(cantidad1 <= cantidad2);
+
+
+
+}
